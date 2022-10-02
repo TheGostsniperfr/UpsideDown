e26a47254d1315f9426edb8223e1488b0a1eb28f
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2022-10-02 17:18:16 +0200

    add gravity switcher
    player smooth rotate

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
@@ -7,19 +8,29 @@ public class playerController : MonoBehaviour
     //component
     [SerializeField] private CharacterController characterController;
 
+    //isGrounded
+    [SerializeField] private float distanceIsGrounded = 1f;
+
+
     //rotation
     [SerializeField] private float mouseSensitivityX = 5f;
     [SerializeField] private float mouseSensitivityY = 5f;
     private float rotationX = 0f;
 
     //walk / jump
-    private Vector3 playerVelocity;
+    [SerializeField] private Vector3 playerVelocity;
     private Vector3 playerInput;
     [SerializeField] private float playerSpeed = 3f;
     [SerializeField] private float playerSprintSpeedMultiplicator = 1.5f;
     [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private float gravity = -9.81f;
     [SerializeField] private bool isSprinting;
+
+    [Header("Gravity switch")]
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private bool gravitySwited;
+
+    //gravity player rotation
+    [SerializeField] private float gravitRotationSpeed = 1f;
 
 
 
@@ -42,9 +53,47 @@ public class playerController : MonoBehaviour
         rotationPlayer();
         rotationCamera();
 
-
+        gravitySwitcher();
 
     }
+
+    private IEnumerator smoothRotation()
+    {
+        Quaternion playerRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+
+        if (gravitySwited)
+        {
+            playerRotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+  
+
+        float time = -0.2f;
+
+        while(time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, playerRotation, time);
+
+            time += Time.deltaTime * gravitRotationSpeed;
+            yield return null;
+        }
+        StopCoroutine(smoothRotation());
+    }
+
+    private bool isGrounded()
+    {
+        if(Physics.Raycast(transform.position, -transform.up, distanceIsGrounded) || characterController.isGrounded)
+        {
+            Debug.Log("IsGrounded");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Is not Grounded");
+            return false;
+        }
+         
+    }
+
 
     private void movePlayer()
     {
@@ -74,17 +123,29 @@ public class playerController : MonoBehaviour
         playerVelocity = (transform.TransformDirection(Vector3.forward) * playerInput.x) + (transform.TransformDirection(Vector3.right) * playerInput.y);
         playerVelocity.y = playerDirectionY;
 
-          
+
 
         //jump system
-        if (characterController.isGrounded)
+        if (isGrounded())
         {
             playerVelocity.y = -1f;
+            if (gravitySwited)
+            {
+                playerVelocity.y = 1f;
+            }
 
-            if (Input.GetKey(KeyCode.Space))
+
+                if (Input.GetKey(KeyCode.Space))
             {
                 Debug.Log("jump");
-                playerVelocity.y = jumpForce;
+                if (!gravitySwited) 
+                {
+                    playerVelocity.y = jumpForce;
+                }
+                else
+                {
+                    playerVelocity.y = -jumpForce;
+                }
             }
         }
         else
@@ -95,6 +156,7 @@ public class playerController : MonoBehaviour
 
         characterController.Move(playerVelocity * Time.deltaTime);
     }
+
 
 
 
@@ -110,5 +172,19 @@ public class playerController : MonoBehaviour
         rotationX -= Input.GetAxis("Mouse Y") * mouseSensitivityY;
         rotationX = Mathf.Clamp(rotationX, camRotMin, camRotMax);
         cam.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+    }
+
+
+    private void gravitySwitcher()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isGrounded())
+        {
+            gravitySwited = !gravitySwited;
+            gravity *= -1;
+            StartCoroutine(smoothRotation());
+         
+
+        }
+        
     }
 }
