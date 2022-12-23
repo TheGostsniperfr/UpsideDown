f967b098f5c4fba28291d4e7fe922020d47715b9
The_Gost_sniper <brianperret.pro@gmail.com>
2022-12-23 10:24:15 +0100

    add graviSphere animation

---
+++ Assets/graviSphereRotation.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class graviSphereRotation : MonoBehaviour
+{
+    [SerializeField] private GameObject ring1;
+    [SerializeField] private GameObject ring4;
+    [SerializeField] private GameObject ring3;
+
+    [SerializeField] private Quaternion newPos1;
+    private float speedX1 = 1f;    
+    private float speedY1 = 1f;    
+    private float speedZ1 = 1f;
+
+    [SerializeField] private Quaternion newPos4;
+    private float speedX4 = 1f;
+    private float speedY4 = 1f;
+    private float speedZ4 = 1f;
+
+    [SerializeField] private Quaternion newPos3;
+    private float speedX3 = 1f;
+    private float speedY3 = 1f;
+    private float speedZ3 = 1f;
+
+
+    private void Start()
+    {
+        newPos1 = ring1.gameObject.transform.localRotation;
+        newPos4 = ring4.gameObject.transform.localRotation;
+        newPos3 = ring3.gameObject.transform.localRotation;
+        newPos4.w = 0.2f;
+
+    }
+
+    private void FixedUpdate()
+    {
+        var t1 = Mathf.Sin(Time.timeSinceLevelLoad);
+        var t4 = Mathf.Sin(Time.timeSinceLevelLoad + 3);
+        var t3 = Mathf.Sin(Time.timeSinceLevelLoad + 7);
+
+
+        if (t1 <= 0.01f && t1 >= -0.01f)
+        {
+            speedX1 = Random.Range(0.2f, 2);
+            speedY1 = Random.Range(0.2f, 2);
+            speedZ1 = Random.Range(0.2f, 2);
+        }
+
+        if (t4 <= 0.01f && t4 >= -0.01f)
+        {
+            speedX4 = Random.Range(0.2f, 2);
+            speedY4 = Random.Range(0.2f, 2);
+            speedZ4 = Random.Range(0.2f, 2);
+        }
+
+
+
+        if (t3 <= 0.01f && t3 >= -0.01f)
+        {
+            speedX3 = Random.Range(0.2f, 2);
+            speedY3 = Random.Range(0.2f, 2);
+            speedZ3 = Random.Range(0.2f, 2);
+        }
+
+        newPos1.x = t1* speedX1;
+        newPos1.y = t1* speedY1; 
+        newPos1.z = t1* speedZ1;
+
+        newPos4.x = t4 * speedX4;
+        newPos4.y = t4 * speedY4;
+        newPos4.z = t4 * speedZ4;
+
+        newPos3.x = t3 * speedX3;
+        newPos3.y = t3 * speedY3;
+        newPos3.z = t3 * speedZ3;
+
+
+        ring1.gameObject.transform.localRotation = newPos1;
+        ring4.gameObject.transform.localRotation = newPos4;
+        ring3.gameObject.transform.localRotation = newPos3;
+
+  
+    }
+
+
+
+}
