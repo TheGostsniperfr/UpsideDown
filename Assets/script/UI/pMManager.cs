fd44078c6077a13a3a399639bf5550b65395f2b8
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-01-08 23:23:32 +0100

    add responsif menu

---
+++ Assets/script/UI/pMManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class pMManager : MonoBehaviour
+{
+    public void resume()
+    {
+        Debug.Log("resume btn clicked");
+    }
+
+    public void setting()
+    {
+        Debug.Log("settings btn clicked");
+    }
+
+    public void menu()
+    {
+        Debug.Log("menu btn clicked");
+    }
+}
--- Assets/script/player/PauseMenu.cs
+++ Assets/script/player/PauseMenu.cs
@@ -5,8 +5,9 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] private GameObject pauseMenuUI;
     [SerializeField] private playerController playerController;
-
     [SerializeField] private bool menuActivate = false;
+    [SerializeField] private GameObject reticule;
+    [SerializeField] private GameObject keyPositions;
 
     private void Awake()
     {
@@ -15,38 +16,70 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-
-
             if (menuActivate)
             {
                 //Exit menu
-                Cursor.visible = false;
-                Cursor.lockState = CursorLockMode.Locked;
-                pauseMenuUI.SetActive(false);
-
-                //enable player mouvement
-                playerController.EnablePlayerInput(true);
-
-                menuActivate = false;
+                removeMenu();
             }
             else
             {
                 //Go to menu
-                Cursor.visible = true;
-                Cursor.lockState = CursorLockMode.None;
-                pauseMenuUI.SetActive(true);
-
-                //stop player mouvement
-                playerController.EnablePlayerInput(false);
-
-
-                menuActivate = true;
+                showMenu();
             }
         }
     }
 
+    private void showMenu()
+    {
+        //remove reticule and keyPosition
+        reticule.SetActive(false);
+        keyPositions.SetActive(false);
 
+        //show cursor
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenuUI.SetActive(true);
+
+        //stop player mouvement
+        playerController.EnablePlayerInput(false);
+
+
+        menuActivate = true;
+    }
+
+    private void removeMenu()
+    {
+        //show reticule and keyPositions
+        reticule.SetActive(true);
+        keyPositions.SetActive(true);
+
+        //remove cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        pauseMenuUI.SetActive(false);
+
+        //enable player mouvement
+        playerController.EnablePlayerInput(true);
+
+        menuActivate = false;
+    }
+
+
+    public void resume()
+    {
+        removeMenu();
+    }
+
+    public void setting()
+    {
+        Debug.Log("settings btn clicked");
+    }
+
+    public void menu()
+    {
+        Debug.Log("menu btn clicked");
+    }
 }
