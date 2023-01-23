9cdc47625f12f28ea9a482220b3b6889058008bb
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-01-23 08:33:39 +0100

    add main menu

---
+++ Assets/script/UI/MainMenuManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void play()
+    {
+        //load first scene
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void settings()
+    {
+        //load settings
+        Debug.Log("settings btn pressed");
+    }
+
+    public void quit()
+    {
+        //quit the game
+        Application.Quit();
+    }
+}
