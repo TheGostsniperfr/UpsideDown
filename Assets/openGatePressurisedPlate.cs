241608b235911b4d3116ff60a3dd1ea04fb82b7e
The_Gost_sniper <brianperret.pro@gmail.com>
2023-02-28 18:49:59 +0100

    add gatePressurisedPlate

---
+++ Assets/openGatePressurisedPlate.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openGatePressurisedPlate : MonoBehaviour
+{
+    public bool playerDetected { get; private set; } = false;
+    public DoorMovement OpenDoor;
+    public string[] listOfTagsToDetect;
+    public int[] listOfLayersToDetect;
+
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        for (int i = 0; i < listOfTagsToDetect.Length; i++)
+        {
+            if (col.gameObject.tag == listOfTagsToDetect[i])
+            {
+                playerDetected = true;
+                openDoor();
+            }
+        }
+
+        for (int j = 0; j < listOfLayersToDetect.Length; j++)
+        {
+            if (col.gameObject.layer == listOfLayersToDetect[j])
+            {
+                playerDetected = true;
+                openDoor();
+            }
+        }        
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        for (int i = 0; i < listOfTagsToDetect.Length; i++)
+        {
+            if (col.gameObject.tag == listOfTagsToDetect[i])
+            {
+                playerDetected = false;
+                closedDoor();
+            }
+        }
+
+        for (int j = 0; j < listOfLayersToDetect.Length; j++)
+        {
+            if (col.gameObject.layer == listOfLayersToDetect[j])
+            {
+                playerDetected = false;
+                closedDoor();
+            }
+        }        
+    }
+
+
+    private void openDoor()
+    {
+        OpenDoor.startTime = Time.time;
+        OpenDoor.TriggerOpeningDoor = true;
+        OpenDoor.TriggerClosingDoor = false;
+    }
+
+    private void closedDoor()
+    {
+        OpenDoor.startTime = Time.time;
+        OpenDoor.TriggerOpeningDoor = false;
+        OpenDoor.TriggerClosingDoor = true;
+    }
+}
