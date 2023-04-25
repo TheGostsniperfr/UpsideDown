48be446f8e50b5c5cebc2966c87dae122685b51f
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-04-25 23:40:17 +0200

    add tuto on salle 2

---
+++ Assets/script/Unity/FindMissingScript.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public static class FindMissingScript
+{
+    [MenuItem("My Menu/Find Missing Scripts In projects")]
+    static void FindMissingScriptsInProjectMenuItem()
+    {
+        string[] prefabPaths = AssetDatabase.GetAllAssetPaths().Where(path => path.EndsWith(".prefab", System.StringComparison.OrdinalIgnoreCase)).ToArray();
+
+        foreach (string path in prefabPaths)
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+
+            foreach (Component component in prefab.GetComponentsInChildren<Component>())
+            {
+                if (component == null)
+                {
+                    Debug.Log("Prefab found with missing script " + path, prefab);
+                    break;
+                }
+            }
+        }
+    }
+
+    [MenuItem("My Menu/Find Missing Scripts In Scene")]
+    static void FindMissingScriptsInSceneMenuItem()
+    {
+        var obj = GameObject.Find("SceneIDMap");
+        Object.DestroyImmediate(obj);
+
+
+        foreach (GameObject gameObject in GameObject.FindObjectsOfType<GameObject>(true))
+        {
+            foreach (Component component in gameObject.GetComponentsInChildren<Component>())
+            {
+                if (component == null)
+                {
+                    Debug.Log("GameObject found with missing script " + gameObject.name, gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
