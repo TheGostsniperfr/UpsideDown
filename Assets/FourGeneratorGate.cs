752d60e25d5d63dd9ce54046e50a643c7e17440a
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2023-06-05 13:58:38 +0200

    Update FourGeneratorGate.cs

--- Assets/FourGeneratorGate.cs
+++ Assets/FourGeneratorGate.cs
@@ -3,7 +3,7 @@ using Mirror;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FourGeneratorGate : MonoBehaviour
+public class FourGeneratorGate : NetworkBehaviour
 {
     [SerializeField] private List<Active_Generator> active_Generators;
     [SerializeField] private DoorMovement doorMovement;
